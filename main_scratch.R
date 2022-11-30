18b583120fd2c95120789b727b394616f7526320
PGszafir <pgszafir@gmail.com>
2022-11-30 23:20:34 +0100

    R scratch instructions hov to run stylo

--- main_scratch.R
+++ main_scratch.R
@@ -1,8 +1,12 @@
 library(stylo)
-
-
 a = c(1,2,3,4,5,6,7,8)
-
 print(a*a)
-
 plot(a*a, col="red")
+
+#hov to run stylo
+
+library(stylo) #import library
+setwd("***\r-stylometry\Corpuses\corpus_a") # set the working directory one upper before corpus
+stylo() # run stylo
+
+#The subdirectory "corpus" should contain at least two text samples!
