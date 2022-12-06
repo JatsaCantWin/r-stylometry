2a748c7800221f00cf677f15d56a88627febb4c5
Piotr Jurek <piotrjurek9807@gmail.com>
2022-12-06 15:59:00 +0100

    feat: basic file download

--- book-scrapper/main.py
+++ book-scrapper/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import requests
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import Book
 
+books = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(1700, 1701):
+    url = "https://www.gutenberg.org/cache/epub/{}/pg{}.txt".format(i, i)
+    print(url)
+    downloadedData = r = requests.get(url)
+    decodedData = downloadedData.text
+    #Author = re.search(r'Author:.*$', decodedData, flags=re.MULTILINE).string
+    Title = re.search("Author:.*", decodedData).string
+    print(type(Title))
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #print(Author)
+    print(Title)
